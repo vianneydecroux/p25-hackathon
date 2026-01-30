45da319b362b433d341e454eeab83b5f8c30ef02
Théo Del Borrello <theo.del_borrello@etu.minesparis.psl.eu>
2026-01-30 10:54:34 +0100

    rajout force +commentaires classes

--- goo.py
+++ goo.py
@@ -1,13 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+class goo:
+    def __init__(self, x, y):
+        self.position = np.array([x,y])
+        self.mass = 0.4
+        self.rayon = 0.01
+        self.vitesse = np.array([0,0])
+        self.force = np.array([0,0]) #liste force x et force y
+        self.liens = dict{}  #entrée liens sortie l0
+    
 
 """Pour travailler directement sur les forces"""
-N = 3
-x_min, x_max = -10., 10.
-y_min, y_max = -10., 10.
-speed_max = 1
 
 X = np.random.uniform(x_min,x_max,N)
 Y = np.random.uniform(y_min,y_max,N)
@@ -17,23 +21,10 @@ VY = np.random.uniform(0,speed_max,N)
 positions = np.concatenate((X,Y)).reshape(2,N)
 speeds = np.concatenate((VX,VY)).reshape(2,N)
 
-dict = {0:[(1,3),(2,3)],1:[(0,3)],2:[(0,3)]}
-
-def forces(position,dict):
-
-
-
-
-
-
-class goo:
-    def __init__(self, x, y):
-        self.position = np.array([x,y])
-        self.mass = 0.4
-        self.rayon = 0.01
-        self.vitesse = np.array([0,0])
-        self.force = np.array([0,0])
-        self.liens = []
-    
-    def ajouter_force
+def forces(goo a,goo b):
+    if b in a.liens.key:
+        d_x=a.position[0]-b.position[0]
+        d_y=a.position[1]-b.position[1]
+        a.force[0]=-k(dx-a.liens[b])
+        a.force[1]=-k(dy-a.liens[b])-a.mass*g
 
