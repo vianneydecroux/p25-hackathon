73500a11a854a388d5b49ae601349931f2459140
Vianney Decroux <vianney.decroux@etu.minesparis.psl.eu>
2026-01-30 14:52:17 +0100

    pas de superposition initialle

--- goo.py
+++ goo.py
@@ -27,16 +27,17 @@ class goo:
         self.vitesse = np.array([0,0], dtype = float)
         self.forces = np.array([0,-self.mass*g], dtype = float) #liste force x et force y
         self.liens = {}  #entrée liens sortie l0
+        self.Prob = False
         if not(self.plateforme):
             for go in Liste_goos:
                 l = np.sqrt( (self.position[0]-go.position[0])**2 + (self.position[1]-go.position[1])**2 )
-                #if l==0: Liste_goos.pop()
+                if l==0: self.Prob = True
                 if  l <= 0.20 and not(go.plateforme):
                     self.liens[go] = l
                     go.liens[self] = l
                 if  l<= 0.10 and go.plateforme:
                     self.liens[go] = l
-            #if self.liens=={}: Liste_goos.pop()
+            if self.liens=={}: self.Prob = True
     
     def update_forces(self):
         self.forces=np.array([0,-self.mass*g])
@@ -44,8 +45,8 @@ class goo:
             d_x=self.position[0]-b.position[0]
             d_y=self.position[1]-b.position[1]
             d=np.sqrt(d_x**2+d_y**2)
-            self.forces[0]+=-k*(d-self.liens[b])*d_x/(d+0.01)
-            self.forces[1]+=-k*(d-self.liens[b])*d_y/(d+0.01)
+            self.forces[0]+=-k*(d-self.liens[b])*d_x/(d+0.1)
+            self.forces[1]+=-k*(d-self.liens[b])*d_y/(d+0.1)
 
 def tdt(t):
     pos=[]
@@ -67,7 +68,11 @@ for i in range (21):
     Liste_goos.append(goo(x_min + i*0.01,0,True))
     Liste_goos.append(goo(x_max - i*0.01,0,True))
 Liste_goos.append(goo(x_min + 0.3,0,False))
+if Liste_goos[-1].Prob:
+    Liste_goos.pop()
 Liste_goos.append(goo(x_min + 0.40,0,False))
+if Liste_goos[-1].Prob:
+    Liste_goos.pop()
 
 fig, ax = plt.subplots()
 ax.set(xlim=[x_min,x_max],ylim=[y_min,y_max])
