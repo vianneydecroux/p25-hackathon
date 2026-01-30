8d3084ecec2d605102568be8fa1d16848ea3e5b1
Vianney Decroux <vianney.decroux@etu.minesparis.psl.eu>
2026-01-30 16:49:41 +0100

    Ajouts de moults commentaires

--- goo.py
+++ goo.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib.backend_bases import MouseButton
 
+#Constantes
 éta=1
 x_min = -0.50
 x_max = 0.50
@@ -11,20 +12,19 @@ y_max = 0.50
 
 g=9.81/10
 k=100
-l0=0.5
 dt=0.01
 liste_goos = []  #vecteur de goos
 
 class Goo:
     def __init__(self, x, y, pl):
-        self.plateforme = pl               #true -> plateforme et pas goo
+        self.plateforme = pl                                    #true -> c'est une plateforme
         self.position = np.array([x,y], dtype = float)
         self.mass = 0.4
         self.rayon = 0.01
         self.vitesse = np.array([0,0], dtype = float)
         self.forces = np.array([0,-self.mass*g], dtype = float) #liste force x et force y
-        self.liens = {}
-        self.Prob = False
+        self.liens = {}                                         #les clés sont les autres goos liés et le contenu et la longueur à vide du ressort
+        self.Prob = False                                       #variable problème : true si superposition de 2 goos où si aucun lien
         if not(self.plateforme):
             liste_plateforme = []
             liste_distance = []
@@ -38,7 +38,7 @@ class Goo:
                     liste_plateforme.append(go)
                     liste_distance.append(l)
             if len(liste_distance) > 0:
-                i = liste_distance.index(min(liste_distance))
+                i = liste_distance.index(min(liste_distance))      #Unicité de l'accroche à la plateforme
                 self.liens[liste_plateforme[i]] = liste_distance[i]
             if self.liens=={}: self.Prob = True
     
@@ -49,7 +49,7 @@ class Goo:
             d_x=self.position[0]-b.position[0]
             d_y=self.position[1]-b.position[1]
             d=np.sqrt(d_x**2+d_y**2)
-            if d > self.liens[b] + 0.02:
+            if d > self.liens[b] + 0.02:                           #Si le ressort vibre trop, il se brise
                 casse.append(b)
             else:
                 self.forces[0]+=-k*(d-self.liens[b])*d_x/(d+0.01)
@@ -64,12 +64,12 @@ class Goo:
             self.liens.pop(b)
             b.liens.pop(self, None)
 
-def on_click(event):
+def on_click(event):                                                #Les clics pour faire apparaitre un nouveau goos
     if event.button is MouseButton.LEFT:
         print(1)
         x,y=(event.xdata,event.ydata)
         liste_goos.append(Goo(x,y,False))
-        if liste_goos[-1].Prob:
+        if liste_goos[-1].Prob:                                     #Si problème, on le supprime directement
             liste_goos.pop()
         else:
             go = liste_goos[-1]
@@ -80,7 +80,7 @@ def on_click(event):
 
 
 
-def tdt(t):
+def tdt(t):                                                         #mise à jour
     pos=[]
     global liste_goos 
     for go in liste_goos :
@@ -93,14 +93,14 @@ def tdt(t):
         pos.append([go.position[0],go.position[1]])
     pos= np.array(pos)
     scat.set_offsets(pos)
-    for (go, go2), line in lines.items():
+    for (go, go2), line in lines.items():                           #dessin des lignes
         line.set_data([go.position[0], go2.position[0]], [go.position[1], go2.position[1]])
 
     return scat, *[l[0] for l in lines]
 
 
 
-"""Initialisation des plateformes"""
+#Initialisation des 2 plateformes
 for i in range (100):
     liste_goos.append(Goo(x_min + i*0.001,0,True))
     liste_goos.append(Goo(x_max - i*0.001,0,True))
@@ -114,7 +114,7 @@ for go in liste_goos:
     pos_y.append(go.position[1])
 
 scat = ax.scatter(pos_x,pos_y,s=30)
-lines = {}
+lines = {}                                                      #Dictionnaire des lignes à tracer
 for go in liste_goos :
     for go2 in go.liens.keys():
         line, = ax.plot([go.position[0], go2.position[0]], [go.position[1], go2.position[1]], c= 'b')
